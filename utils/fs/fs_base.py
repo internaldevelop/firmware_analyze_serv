8b25b97ebafbb97f0a3c7a0eea3e55680c89547a
lvjz2018 <lvjianzhang@sina.com>
2020-04-23 16:39:20 +0800

    romfs extract

--- utils/fs/fs_base.py
+++ utils/fs/fs_base.py
@@ -5,6 +5,9 @@ from utils.gadget.my_file import MyFile
 from utils.gadget.my_path import MyPath
 from utils.gadget.strutil import StrUtils
 
+# import magic
+import codecs
+
 
 class FsBase(object):
     # ============================================================
@@ -68,5 +71,63 @@ class FsBase(object):
 
         return file_type, None
 
+    @staticmethod
+    def is_binary_file_1(file_content):
+        '''
+        根据text文件数据类型判断是否是二进制文件
+        :param ff: 文件名（含路径）
+        :return: True或False，返回是否是二进制文件
+        '''
+        TEXT_BOMS = (
+            codecs.BOM_UTF16_BE,
+            codecs.BOM_UTF16_LE,
+            codecs.BOM_UTF32_BE,
+            codecs.BOM_UTF32_LE,
+            codecs.BOM_UTF8,
+        )
+        #: BOMs to indicate that a file is a text file even if it contains zero bytes.
+        return not any(file_content.startswith(bom) for bom in TEXT_BOMS) and b'\0' in file_content
 
+    @staticmethod
+    def is_binwary_file_2(file_content):
+        # '''
+        # 根据magic文件的魔术判断是否是二进制文件
+        # :param ff: 文件名（含路径）
+        # :return: True或False，返回是否是二进制文件
+        # '''
+        # mime_kw = 'x-executable|x-sharedlib|octet-stream|x-object'  ###可执行文件、链接库、动态流、对象
+        # try:
+        #     magic_mime = magic.from_file(ff, mime=True)
+        #     magic_hit = re.search(mime_kw, magic_mime, re.I)
+        #     if magic_hit:
+        #         return True
+        #     else:
+        #         return False
+        # except Exception, e:
+        #     return False
+        pass
+
+    # 判断文件是否是elf文件
+    @staticmethod
+    def is_ELFfile(file_content):
+        # if not os.path.exists(filepath):
+        #     logger.info('file path {} doesnot exits'.format(filepath))
+        #     return False
+        # # 文件可能被损坏，捕捉异常
+        try:
+            # FileStates = os.stat(filepath)
+            # FileMode = FileStates[stat.ST_MODE]
+            # if not stat.S_ISREG(FileMode) or stat.S_ISLNK(FileMode):  # 如果文件既不是普通文件也不是链接文件
+            #     return False
+            header = (bytearray(file_content)[1:4]).decode(encoding="utf-8")
+            # logger.info("header is {}".format(header))
+            if header in ["ELF"]:
+                # print header
+                return True
+        except UnicodeDecodeError as e:
+            # logger.info("is_ELFfile UnicodeDecodeError {}".format(filepath))
+            # logger.info(str(e))
+            pass
+
+        return False
 
--- utils/fs/img_romfs.py
+++ utils/fs/img_romfs.py
@@ -1,8 +1,7 @@
 import os
-from PySquashfsImage import SquashFsImage
 
-from jefferson import jffs2_lzma, rtime
-from .jffs2 import JFFS2
+from PyRomfsImage.PyRomfsImage import Romfs
+
 from utils.fs.fs_base import FsBase
 from utils.const.file_type import FileType
 from utils.gadget.file_type_scan.exec_file import ExecFile
@@ -10,29 +9,11 @@ from utils.gadget.file_type_scan.file_type_judge import FileTypeJudge
 from utils.gadget.my_file import MyFile
 from utils.gadget.my_path import MyPath
 
-JFFS2_NODETYPE_DIRENT = 0xE001
-JFFS2_NODETYPE_INODE = 0xE002
 
-DT_DIR = 4
-DT_REG = 8
-
-JFFS2_COMPR_NONE = 0x00
-JFFS2_COMPR_ZERO = 0x01
-JFFS2_COMPR_RTIME = 0x02
-JFFS2_COMPR_RUBINMIPS = 0x03
-JFFS2_COMPR_COPY = 0x04
-JFFS2_COMPR_DYNRUBIN = 0x05
-JFFS2_COMPR_ZLIB = 0x06
-JFFS2_COMPR_LZO = 0x07
-
-
-class IMG_JFFS2(FsBase):
-    """
-    参考：# https://github.com/luizluca/jffs2
-    """
+class IMG_RomFS(FsBase):
     def __init__(self, file_path):
         self.image = None
-        # 加载JFFS2镜像文件，如果非 JFFS2 格式，则 image 为 None
+        # 加载 squash-fs 的镜像文件，如果非 squash-fs 格式，则 image 为 None
         self.open(file_path)
 
     def list_all(self, exclude_folder=False, exclude_file=False):
@@ -41,7 +22,7 @@ class IMG_JFFS2(FsBase):
             return []
 
         nodes = []
-        for inode in self.image.inodes:
+        for inode in self.image.getRoot().findAll():
             nodes.append(inode)
             # if len(inode.getName()) == 0:
             #     # 忽略根节点
@@ -54,8 +35,8 @@ class IMG_JFFS2(FsBase):
             #     continue
             # else:
             #     nodes.append(inode)
-            #     # print(inode.getName())
-            #     # print(inode.getPath())
+                # print(inode.getName())
+                # print(inode.getPath())
 
         return nodes
 
@@ -92,37 +73,25 @@ class IMG_JFFS2(FsBase):
             return FileType.NORMAL_FILE, None
 
     def extract_files(self, extract_func=None):
-
         # 导出文件内容，忽略目录，导出方式由 extract_func 来进行
-        self.image.scan()
-        total_count = len(self.image.dirents)
-
-        for index in self.image.dirents:
-            dirent = self.image.dirents[index]
-            name, ntype = self.image.resolve_dirent(index)
-            # mode = self.image.fd.mode
+        # nodes = self.list_all(exclude_folder=True)
+        root = self.image.getRoot()
+        index = 0
+        for inode in root.findAll():
+            index += 1
+            content = root.read(inode)
+            # name = os.path.basename(inode)
+            name = str(inode)
+            path = os.path.splitext(inode)
             mode = 0o111
 
-            if ntype == DT_REG:
-                # image.dump_file(os.path.join(target, name), i)
-                content = self.image.get_file_content(name, index)
-                file_type, extra_props = IMG_JFFS2._file_type(name, mode, content, True)
+            file_type, extra_props = IMG_RomFS._file_type(name, mode, content, True)
 
-                # 属性和数据内容交由 extract_func 回调函数处理
-                if extract_func is not None:
-                    path = os.path.splitext(name)
-                    ret = extract_func(name, path, file_type, content, index, total_count, extra_props=extra_props)
-                    if not ret:
-                        return False
-            # elif ntype == DT_DIR:
-            #     try:
-            #         # mkdir_p(os.path.join(target, name))
-            #         print("mkdir")
-            #     except OSError as e:
-            #         print(e)
-
-        # 全部处理完毕后，返回 True 表示正常处理完成。
-        return True
+            # print(('EXEC_FILE({})' if file_type == 4 else 'NORMAL_FILE({})').format(inode.inode.mode))
+            # 属性和数据内容交由 extract_func 回调函数处理
+            if extract_func is not None:
+                total_count = len(root.findAll())
+                extract_func(name, path, file_type, content, index, total_count, extra_props=extra_props)
 
     def check_format(self):
         # 检测加载的镜像，是否为有效的 squash-fs 格式
@@ -131,10 +100,9 @@ class IMG_JFFS2(FsBase):
     def open(self, file_path):
         try:
             # 加载并解析镜像
-            self.image = JFFS2(file_path)
-
+            self.image = Romfs()
+            self.image.open(file_path)
         except OSError as e:
-            print(e)
             # 格式错误时，镜像对象设置为无效
             self.image = None
 
@@ -146,6 +114,3 @@ class IMG_JFFS2(FsBase):
 
     def __del__(self):
         self.close()
-
-
-
